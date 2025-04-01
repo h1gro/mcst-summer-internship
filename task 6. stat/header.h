60448543ab68e0802c3653bc717a2ceaad02f629
h1gro <komarov.aa@phystech.edu>
2025-04-01 21:59:18 +0300

    time information

--- task 6. stat/header.h
+++ task 6. stat/header.h
@@ -1,6 +1,7 @@
 #ifndef _STAT_
 #define _STAT_
 
+//macros for defining st_mode file
 #define _DEF_TYPE_(_MACR_NAME_, MESSAGE) \
 {                                        \
 if (_MACR_NAME_(file_inf->st_mode))      \
@@ -8,8 +9,9 @@ if (_MACR_NAME_(file_inf->st_mode))      \
     printf(MESSAGE);                     \
     return TYPE_EXIST;                   \
 }                                        \
-}while(0);
+}while(0); //while(0); - need for writing symbol ';' at the end of macroline
 
+//consts for func define_type(...)
 enum ret_consts
 {
     TYPE_EXIST     = 0,
@@ -17,5 +19,7 @@ enum ret_consts
 };
 
 ret_consts define_type       (struct stat* file_inf);
+
+void printf_file_information (struct stat* file_inf);
 
 #endif
--- task 6. stat/main.c
+++ task 6. stat/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <assert.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #include "header.h"
 
 int main(int argc, char *argv[])
 {
+    struct stat file_inf = {0};  //init structure
 
-    struct stat file_inf = {0};
-
-    if (stat(argv[1], &file_inf) == -1)
+    if (stat(argv[1], &file_inf) == -1) //check stat return for find error in file
     {
         fprintf(stderr, "Error in file\n");
         return 0;
     }
 
-    printf("File name: %s\n\n", argv[1]);
+    printf("File name: %s\n\n", argv[1]); //printf file name, argv[1] = probfile.txt
 
-    if (define_type(&file_inf) == TYPE_NOT_EXIST)
+    if (define_type(&file_inf) == TYPE_NOT_EXIST) //check return varyable from define_type
     {
         fprintf(stderr, "<<<Type of the file is not exist in stat lib!>>>\n");
     }
+
+    printf_file_information(&file_inf); //func for print file parametres
 
     return 0;
 }
@@ -29,6 +31,7 @@ ret_consts define_type (struct stat* file_inf)
 {
     assert(file_inf);
 
+    //define type of the file with help of standart macrosses
     _DEF_TYPE_(S_ISBLK,  "The type is block special file\n");
     _DEF_TYPE_(S_ISCHR,  "The type is character special file\n");
     _DEF_TYPE_(S_ISDIR,  "The type is directory\n");
@@ -37,6 +40,16 @@ ret_consts define_type (struct stat* file_inf)
     _DEF_TYPE_(S_ISREG,  "The type is ordinary file\n");
     _DEF_TYPE_(S_ISSOCK, "The type is soket file\n");
 
-    return TYPE_NOT_EXIST;
+    return TYPE_NOT_EXIST;  //const for error type
 }
 
+void printf_file_information(struct stat* file_inf)
+{
+    assert(file_inf);
+
+    //print file parametres
+    printf("File bytes size: %ld\nLast open was: %s"
+           "Last modification: %sLast changes: %s",
+           file_inf->st_size, ctime(&file_inf->st_atime),
+           ctime(&file_inf->st_mtime), ctime(&file_inf->st_ctime));
+}
